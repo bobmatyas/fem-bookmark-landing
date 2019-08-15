58a03d2ee620404992b662246288b20c74718821
bobmatyas <ballium@protonmail.com>
2019-08-14 21:19:04 -0500

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const gulp = require("gulp");
+
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const sass = require('gulp-sass');
+
+
+const minify = _ => gulp.src("scripts/scripts.js").pipe(uglify()).pipe(gulp.dest("scripts/min"));
+
+gulp.task('sass', function(done) {
+  gulp.src('./css/sass/*.scss')
+      .pipe(sass())
+      .pipe(concat('styles.css')) 
+      .pipe(gulp.dest('./css/'));
+      done();
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./css/sass/*.scss', gulp.series('sass'));
+});
+
+gulp.task("minify", minify);
